a5da747f635f8283206d3ac4a0bf091285de82f1
nusaibatron <nusaiba@berkeley.edu>
2022-05-03 17:49:25 -0400

    forward and scan

--- src/Mail.js
+++ src/Mail.js
@@ -4,20 +4,33 @@ import recipient from './recipient.svg';
 
 
 const Mail = (mail) => {
+  console.log(mail)
+
   const { id, businessRecipient, from, forward, imageUrl, individualRecipient, scan, timestamp} = mail;
+  mail = JSON.parse(JSON.stringify(mail));
+  console.log(mail)
+
+  if ("scan" in mail == false) {
+    let s = {status: ''}
+    mail["scan"] = s;
+  }
+
+  if ("forward" in mail == false) {
+    let s = {status: ''};
+    mail["forward"] = s;
+  }
+  const fwd = mail.forward.status
+  const sc = mail.scan.status
+
   const date = new Date(timestamp).toLocaleDateString("en-US");
   return (
     <article className="card">
       <img src={imageUrl} alt={id} />
       <h5>{from}</h5>
-      {/* <h5 > ID: </h5> <h6>{id}</h6> */}
-      <icon>
-      {/* <img src={company}/> */}
       <h6>{businessRecipient}</h6>
-      </icon>
-      {/* // <h4> Forward: {forward}</h4> */}
+      <h6> Scan: {sc}</h6>
+      <h6> Forward: {fwd}</h6>
       <h6>{individualRecipient}</h6>
-      {/* // <h4> Scan: {scan}</h4> */}
       <footer>{date}</footer>
 
     </article>
