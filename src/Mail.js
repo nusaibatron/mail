38568ac0a11ef333c6c0cbb3316f6f9bf98a5671
nusaibatron <nusaiba@berkeley.edu>
2022-05-03 23:54:25 -0400

    scan and forward

--- src/Mail.js
+++ src/Mail.js
@@ -9,18 +9,26 @@ const Mail = (mail) => {
   const { id, businessRecipient, from, forward, imageUrl, individualRecipient, scan, timestamp} = mail;
   mail = JSON.parse(JSON.stringify(mail));
   console.log(mail)
+  let stat = "";
+  let frwd = "";
 
   if ("scan" in mail == false) {
     let s = {status: ''}
     mail["scan"] = s;
+    stat = "";
+  } else {
+    stat = "Scan: "
   }
 
   if ("forward" in mail == false) {
     let s = {status: ''};
     mail["forward"] = s;
+    frwd = "";
+  } else {
+    frwd = "Forward: "
   }
-  const fwd = mail.forward.status
-  const sc = mail.scan.status
+  const fwd = frwd + mail.forward.status
+  const sc = stat + mail.scan.status
 
   const date = new Date(timestamp).toLocaleDateString("en-US");
   return (
@@ -28,8 +36,8 @@ const Mail = (mail) => {
       <img src={imageUrl} alt={id} />
       <h5>{from}</h5>
       <h6>{businessRecipient}</h6>
-      <h6> Scan: {sc}</h6>
-      <h6> Forward: {fwd}</h6>
+      <h6> {sc}</h6>
+      <h6> {fwd}</h6>
       <h6>{individualRecipient}</h6>
       <footer>{date}</footer>
 
